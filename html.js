60bfaf2cad80271a9cb80d7a6d3c23df8a50973d
Fareslopez <fhernandezlopez89263@uat.edu>
2024-12-01 22:36:49 -0700

    Add files via upload

---
+++ html.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Demo DJ</title>
+        <style>
+            body{
+                padding-top: 500px;
+                justify-content: center;
+                min-height: 100vh;
+                margin: 0;
+                align-items: center;
+                background-color: blue;
+                font-family: Arial, Helvetica, sans-serif;
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="music"player-container></div>
+        <h1>Music</h1>
+
+        <audio controls preload="auto">
+        <source src="Living Without Fear.mp3" type="audio/mpeg">
+            Your browser doesn not support the audio element.
+        </audio>
+    </body>
+</html>
